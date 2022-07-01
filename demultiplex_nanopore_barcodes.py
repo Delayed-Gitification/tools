2312c35854de7e8d8b62cd920df9ae6c01a271db
Oscar Wilkins <64151406+Delayed-Gitification@users.noreply.github.com>
2022-07-01 22:24:58 +0100

    Update demultiplex_nanopore_barcodes.py

--- demultiplex_nanopore_barcodes.py
+++ demultiplex_nanopore_barcodes.py
@@ -28,13 +28,18 @@ def find_barcode(seq, barcodes, min_score, max_ambiguity, stored_results):
 	scores = []
 	names = []
 
+	# print(seq)
+	# print(barcodes)
+
 	if seq in stored_results.keys():
-		return stored_results[seq]
+		return stored_results[seq], stored_results
 
 	for name, bc in barcodes.items():
 		score = fast_fuzz(seq, bc)
 		scores.append(score)
 		names.append(name)
+
+	#print(scores)
 
 	# Reject if similar to >1 barcode (ambiguous)
 	if len([a for a in scores if a > max_ambiguity]) > 1:
@@ -45,6 +50,8 @@ def find_barcode(seq, barcodes, min_score, max_ambiguity, stored_results):
 
 	else:
 		stored_results[seq] = -1
+
+	#print(stored_results[seq])
 	
 	return stored_results[seq], stored_results
 
@@ -75,8 +82,8 @@ def main():
 	parser.add_argument("-f", "--fastq", required=True)
 	parser.add_argument("-p", "--primers", required=True, help="This should be a headerless, three column csv with columns 1=name,2=barcode,3=f or r")
 	parser.add_argument("-s", "--min_score", type=float, default=92, help="When matching barcodes")
-	parser.add_argument("--max_ambiguity", type=float, default=76, help="If another barcode has this score or higher, then ignore this read because it's ambiguous")
-	parser.add_argument("-l", "--length", type=int, default=35, help="length to look at start and end of reads "
+	parser.add_argument("--max_ambiguity", type=float, default=82, help="If another barcode has this score or higher, then ignore this read because it's ambiguous")
+	parser.add_argument("-l", "--length", type=int, default=80, help="length to look at start and end of reads "
 	                                                                 "for barcode")
 	parser.add_argument("--ignore_rc", action="store_true", default=False, help="by default it looks at the read and its reverse complement. Use this command to only look at the forward read")
 	parser.add_argument("-o", "--output", required=True)
@@ -89,8 +96,8 @@ def main():
 	reverse_primers = {}
 	with open(args.primers) as file:
 		for i, line in enumerate(file):
-			if i == 0:
-				continue
+			# if i == 0:
+			# 	continue
 
 			line2 = line.rstrip().split(",")
 
@@ -117,12 +124,21 @@ def main():
 	print(reverse_primers)
 	print(to_write_d)
 
+
+
 	stored_results1 = {}
 	stored_results2 = {}
 
 	with dnaio.open(args.fastq) as file:
 		since_written = 0
 		for i, record in enumerate(file):
+
+			if record.name != "b5ecdfbe-c4f8-43be-9e21-068d44fe1fbc runid=650322ee6ff6330f864d1de390f0ea5aa7cf3d6d sampleid=r3 read=16038 ch=23 start_time=2022-07-01T17:09:30Z model_version_id=2021-05-17_dna_r9.4.1_minion_384_d37a2ab9":
+				continue
+
+			if i % 1000 == 0 and i > 0:
+				print(i)
+				#break
 			forward_results = []
 			reverse_results = []
 			for rc in [True, False]:
@@ -140,11 +156,15 @@ def main():
 
 				bc1, stored_results1 = find_barcode(s1, forward_primers, args.min_score, args.max_ambiguity, stored_results1)
 				bc2, stored_results2 = find_barcode(s2, reverse_primers, args.min_score, args.max_ambiguity, stored_results2)
+				#bc2 = -1
 
 				if rc:
 					reverse_results = [bc1, bc2]
 				else:
 					forward_results = [bc1, bc2]
+
+			#print(forward_results)
+			#print(reverse_results)
 
 			if args.ignore_rc:
 				if -1 in forward_results:
@@ -152,16 +172,17 @@ def main():
 				bc1 = forward_results[0]
 				bc2 = forward_results[1]
 			else:
-				if forward_results == [-1, -1] and -1 not in reverse_results:
+				if forward_results == [-1, -1] and reverse_results != [-1, -1]:
 					# reverse results are good
 					bc1 = reverse_results[0]
 					bc2 = reverse_results[1]
-				elif -1 not in forward_results and reverse_results == [-1, -1]:
+				elif forward_results != [-1, -1] and reverse_results == [-1, -1]:
 					bc1 = forward_results[0]
 					bc2 = forward_results[1]
 
 
-
+			#print(bc1)
+			#print(bc2)
 			to_write_d[str(bc1) + "_" + str(bc2)] += "\n".join(["@"+record.name, record.sequence, "+", record.qualities]) + "\n"
 			since_written += 1
 
@@ -171,7 +192,7 @@ def main():
 				since_written = 0
 				print(i)
 
-		write_out_d(to_write_d, args.output)
+	write_out_d(to_write_d, args.output)
 
 
 
