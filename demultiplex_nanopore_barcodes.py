141a0760a1e0ae80ffa052e3f1339c8e815e052f
Oscar Wilkins <64151406+Delayed-Gitification@users.noreply.github.com>
2022-07-01 22:35:53 +0100

    Update demultiplex_nanopore_barcodes.py

--- demultiplex_nanopore_barcodes.py
+++ demultiplex_nanopore_barcodes.py
@@ -28,8 +28,8 @@ def find_barcode(seq, barcodes, min_score, max_ambiguity, stored_results):
 	scores = []
 	names = []
 
-	# print(seq)
-	# print(barcodes)
+	#print(seq)
+	#print(barcodes)
 
 	if seq in stored_results.keys():
 		return stored_results[seq], stored_results
@@ -42,16 +42,21 @@ def find_barcode(seq, barcodes, min_score, max_ambiguity, stored_results):
 	#print(scores)
 
 	# Reject if similar to >1 barcode (ambiguous)
+	#print(max_ambiguity)
 	if len([a for a in scores if a > max_ambiguity]) > 1:
+		#print([a for a in scores if a > max_ambiguity])
 		stored_results[seq] = -1
+		#print("ambiguous")
 
 	elif max(scores) >= min_score:
 		stored_results[seq] = names[scores.index(max(scores))]
+		#print("yeh!")
 
 	else:
 		stored_results[seq] = -1
+		#print("nah")
 
-	#print(stored_results[seq])
+	##print(stored_results[seq])
 	
 	return stored_results[seq], stored_results
 
@@ -87,6 +92,7 @@ def main():
 	                                                                 "for barcode")
 	parser.add_argument("--ignore_rc", action="store_true", default=False, help="by default it looks at the read and its reverse complement. Use this command to only look at the forward read")
 	parser.add_argument("-o", "--output", required=True)
+	parser.add_argument("--chunk", default=500, type=int)
 	args = parser.parse_args()
 
 	assert args.min_score >= args.max_ambiguity
@@ -120,9 +126,9 @@ def main():
 
 	to_write_d = initialise_d(forward_primers, reverse_primers)
 
-	print(forward_primers)
-	print(reverse_primers)
-	print(to_write_d)
+	#print(forward_primers)
+	#print(reverse_primers)
+	#print(to_write_d)
 
 
 
@@ -133,8 +139,8 @@ def main():
 		since_written = 0
 		for i, record in enumerate(file):
 
-			if record.name != "b5ecdfbe-c4f8-43be-9e21-068d44fe1fbc runid=650322ee6ff6330f864d1de390f0ea5aa7cf3d6d sampleid=r3 read=16038 ch=23 start_time=2022-07-01T17:09:30Z model_version_id=2021-05-17_dna_r9.4.1_minion_384_d37a2ab9":
-				continue
+			# if record.name != "b5ecdfbe-c4f8-43be-9e21-068d44fe1fbc runid=650322ee6ff6330f864d1de390f0ea5aa7cf3d6d sampleid=r3 read=16038 ch=23 start_time=2022-07-01T17:09:30Z model_version_id=2021-05-17_dna_r9.4.1_minion_384_d37a2ab9":
+			# 	continue
 
 			if i % 1000 == 0 and i > 0:
 				print(i)
@@ -156,6 +162,7 @@ def main():
 
 				bc1, stored_results1 = find_barcode(s1, forward_primers, args.min_score, args.max_ambiguity, stored_results1)
 				bc2, stored_results2 = find_barcode(s2, reverse_primers, args.min_score, args.max_ambiguity, stored_results2)
+				##print(bc2)
 				#bc2 = -1
 
 				if rc:
@@ -163,8 +170,8 @@ def main():
 				else:
 					forward_results = [bc1, bc2]
 
-			#print(forward_results)
-			#print(reverse_results)
+			##print(forward_results)
+			##print(reverse_results)
 
 			if args.ignore_rc:
 				if -1 in forward_results:
@@ -181,16 +188,16 @@ def main():
 					bc2 = forward_results[1]
 
 
-			#print(bc1)
-			#print(bc2)
+			##print(bc1)
+			##print(bc2)
 			to_write_d[str(bc1) + "_" + str(bc2)] += "\n".join(["@"+record.name, record.sequence, "+", record.qualities]) + "\n"
 			since_written += 1
 
-			if since_written == 10_000:
+			if since_written == args.chunk:
 				write_out_d(to_write_d, args.output)
 				to_write_d = initialise_d(forward_primers, reverse_primers)
 				since_written = 0
-				print(i)
+				##print(i)
 
 	write_out_d(to_write_d, args.output)
 
